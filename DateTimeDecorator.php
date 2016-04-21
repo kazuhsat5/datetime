7897f1c92b7bcb5e615d0e1af89f48d39320f21e
kazuhsat <kazuhsat@MAC-B-175.local>
2016-04-21 14:43:42 +0900

    mod

--- DateTimeDecorator.php
+++ DateTimeDecorator.php
@@ -10,10 +10,10 @@ class DateTimeDecorator extends DateTime
     /**
      * 比較する日付との秒差を取得する
      *
-     * @param DateTime $dateTime DateTimeオブジェクト
+     * @param DateTimeInterface $dateTime
      * @return integer
      */
-    public function getMarginSecond(DateTime $dateTime)
+    public function getMarginSecond(DateTimeInterface $dateTime): int
     {
         return $dateTime->getTimestamp() - $this->getTimestamp();
     }
--- example.php
+++ example.php
@@ -5,6 +5,6 @@ require __DIR__ . '/DateTimeDecorator.php';
 date_default_timezone_set("Asia/Tokyo");
 
 $now      = new DateTimeDecorator();
-$tomorrow = new DateTime('00:00:00 +1 day');    # 明日の0時
+$tomorrow = new DateTimeImmutable('00:00:00 +1 day');    # 明日の0時
 
 echo 'diff:' . $now->getMarginSecond($tomorrow) . PHP_EOL;
