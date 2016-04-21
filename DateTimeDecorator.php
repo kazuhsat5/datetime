9076a8915f1a10129fb9710efc754224ab2f6ad2
佐藤　和大 <kazuhsat@MBP-15JAC-671.local>
2016-04-21 14:57:59 +0900

    mod

--- DateTimeDecorator.php
+++ DateTimeDecorator.php
@@ -8,13 +8,30 @@
 class DateTimeDecorator extends DateTime
 {
     /**
+     * DateTimeInterface
+     *
+     * @var DateTimeInterface
+     */
+    private $_dateTime;
+
+    /**
+     * コンストラクタ
+     *
+     * @param DateTimeInterface $dateTimeInterface
+     */
+    public function __construct(DateTimeInterface $dateTime)
+    {
+        $this->_dateTime = $dateTime;
+    }
+
+    /**
      * 比較する日付との秒差を取得する
      *
-     * @param DateTime $dateTime DateTimeオブジェクト
+     * @param DateTime $dateTime
      * @return integer
      */
-    public function getMarginSecond(DateTime $dateTime)
+    public function getMarginSecond(DateTime $dateTime): int
     {
-        return $dateTime->getTimestamp() - $this->getTimestamp();
+        return $dateTime->getTimestamp() - $this->_dateTime->getTimestamp();
     }
 }
--- example.php
+++ example.php
@@ -4,7 +4,7 @@ require __DIR__ . '/DateTimeDecorator.php';
 
 date_default_timezone_set("Asia/Tokyo");
 
-$now      = new DateTimeDecorator();
+$now      = new DateTimeDecorator(new DateTimeImmutable());
 $tomorrow = new DateTime('00:00:00 +1 day');    # 明日の0時
 
 echo 'diff:' . $now->getMarginSecond($tomorrow) . PHP_EOL;
